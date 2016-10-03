99a83d604a0010d298efd08c8a1fe798d4ef1d27
Gunter Zeilinger <gunterze@gmail.com>
2016-10-03 10:08:01 +0200

    support host volumns /opt/ats/etc/trafficserver and /opt/ats/var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,13 @@
 set -e
 
 if [ "$1" = 'traffic_cop' ]; then
-    if [ ! -d $ATS_HOME/etc ]; then
+    if [ ! -f $ATS_HOME/etc/trafficserver/records.config ]; then
         cp -r /docker-entrypoint.d/etc $ATS_HOME
         chown -R tserver:tserver $ATS_HOME/etc
+    fi
+    if [ ! -d $ATS_HOME/var/log/trafficserver ]; then
+        mkdir -p $ATS_HOME/var/trafficserver $ATS_HOME/var/log/trafficserver
+        chown -R tserver:tserver $ATS_HOME/var
     fi
 fi
 
